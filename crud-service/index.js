7e460731a8414258cc918e3376bc8c3446e5ea8a
Rohit <rohit192ranjan@gmail.com>
2023-06-22 17:51:15 +0530

    Deployment ready 1.1

--- crud-service/index.js
+++ crud-service/index.js
@@ -31,9 +31,9 @@ const PORT = process.env.PORT;
 Connection(USERNAME, PASSWORD);
 
 //static files
-app.use(express.static(path.join(__dirname, './crud-app/build')));
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, "./crud-app/build/index.html"));
-});
+app.use(express.static(path.join(__dirname, '/crud-app/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'crud-app', 'build', 'index.html'))
+);
  
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
